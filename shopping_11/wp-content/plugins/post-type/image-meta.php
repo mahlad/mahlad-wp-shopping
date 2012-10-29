ba2bca7d8b836dc7ef5ac1668298d354137be7e3
mahlad <mahlad365@gmail.com>
2012-10-29 21:16:22 +0330

    a plugin for upload the image and file in a post type

---
+++ shopping_11/wp-content/plugins/post-type/image-meta.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+Plugin Name: Upload Metaboxe
+Plugin URI: 
+Description: 
+Author: Mahla Delpak
+Version: 1.2
+Author URI: https://github.com/mahlad 
+*/
+/*--------Upload Metaboxe---------*/
+
+add_action('post_edit_form_tag', 'update_edit_form'); 
+add_action('load-post.php', 'metabox_setup' );
+add_action('load-post-new.php', 'metabox_setup' );
+add_action('save_post', 'metabox_save', 10, 2 );
+
+
+function update_edit_form() {  
+    echo ' enctype="multipart/form-data"';  
+}
+
+function metabox_setup() {
+	add_meta_box(
+		'gsp_post_meta',
+		'لوگوی شرکت',	
+		'metabox_content',		
+		'marketting',				
+		'advanced',		
+		'high'	
+	);
+	add_meta_box(
+		'gsp_post_meta2',
+		'فایل متن کامل',	
+		'metabox_content',		
+		'learn',				
+		'advanced',		
+		'high'	
+	);
+
+}
+
+function metabox_content($post){
+	$profile_picture =  get_post_meta( $post->ID, 'profile_picture', true );
+	if($profile_picture['url']){
+		$img = $profile_picture['url'];
+		echo '<div id="profile_picture"><a href="'.$img.'" target="_blank"><img src="'.$img.'"></a>
+		</div>
+		<br />';	
+	}
+
+?>
+	<input id="wp_custom_attachment" name="wp_custom_attachment" value="" size="25" type="file">
+ <?php
+}
+
+
+function metabox_save($post_id){
+	if(!empty($_FILES['wp_custom_attachment']['name'])) {	
+		$supported_types = array('image/gif','image/bmp','image/jpeg','image/png','application/pdf','application/msword');  		
+		$arr_file_type = wp_check_filetype(basename($_FILES['wp_custom_attachment']['name']));  
+		$uploaded_type = $arr_file_type['type'];  
+		if(in_array($uploaded_type, $supported_types)) {  
+			$upload = wp_upload_bits($_FILES['wp_custom_attachment']['name'], null, file_get_contents($_FILES['wp_custom_attachment']['tmp_name'])); 
+			if(isset($upload['error']) && $upload['error'] != 0) {  
+				//wp_die('There was an error uploading your file. The error is: ' . $upload['error']);  
+			} else {
+				unset($upload['error']);
+				$upload['file'] = str_replace(chr(92),"/",$upload['file']);
+				update_post_meta($post_id ,'profile_picture', $upload);  
+			}
+		} else {
+			//wp_die("The file type that you've uploaded is not a PDF.");  
+		}
+	} 
+}
